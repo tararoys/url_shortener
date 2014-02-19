acb34e93f8c25a6671d4021f4292abb136afef0b
thejessleigh <j.l.unrein@gmail.com>
2014-02-19 10:16:24 -0600

    created a post '/urls' path in the controller. Have a primative before_save callback working.

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -1,4 +1,9 @@
 get '/' do
-
   erb :index
+end
+
+post '/urls' do
+  ShortUrl.create(longform: params[:longform])
+
+  erb :confirmation
 end
--- sinatra_skeleton/app/models/short_url.rb
+++ sinatra_skeleton/app/models/short_url.rb
@@ -1,3 +1,11 @@
 class ShortUrl < ActiveRecord::Base
 
+  before_save :generate_shortform
+
+  def generate_shortform
+    if shortform.nil?
+      self.shortform = ShortUrl.all.last.id + 1
+    end
+  end
+
 end
