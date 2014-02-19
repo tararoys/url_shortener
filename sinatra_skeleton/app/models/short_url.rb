c9694c31a469441b5412c0de184893655afae87f
thejessleigh <j.l.unrein@gmail.com>
2014-02-19 10:31:07 -0600

    refactored generate_shortform method
    instead of assigning the id element to the shortform column, we are now
    generating a random string and checking it against the database for uniqueness.

--- sinatra_skeleton/app/models/short_url.rb
+++ sinatra_skeleton/app/models/short_url.rb
@@ -4,7 +4,11 @@ class ShortUrl < ActiveRecord::Base
 
   def generate_shortform
     if shortform.nil?
-      self.shortform = ShortUrl.all.last.id + 1
+      random_string = (0...50).map { ('a'..'z').to_a[rand(26)] }.sample(6).join
+      while ShortUrl.where(shortform: random_string).length != 0
+        random_string = (0...50).map { ('a'..'z').to_a[rand(26)] }.sample(6).join
+      end
+      self.shortform = random_string
     end
   end
 
