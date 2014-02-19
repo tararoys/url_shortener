17478d983115abc48a63560d91ffc14b083290ae
thejessleigh <j.l.unrein@gmail.com>
2014-02-19 08:58:23 -0600

    fixed some issues in the short_urls migration file. Added seed data.

--- sinatra_skeleton/db/migrate/20140219084624_create_short_urls.rb
+++ sinatra_skeleton/db/migrate/20140219084624_create_short_urls.rb
@@ -1,9 +1,10 @@
 class CreateShortUrls < ActiveRecord::Migration
   def change
-    create_table short_urls do |t|
+    create_table :short_urls do |t|
       t.string :long
       t.string :short
 
       t.timestamps
+    end
   end
 end
---
+++ sinatra_skeleton/db/seeds.rb
@@ -0,0 +1,2 @@
+ShortUrl.create(long: 'http://www.google.com', short: 'google')
+ShortUrl.create(long: 'https://www.github.com', short: 'git')
